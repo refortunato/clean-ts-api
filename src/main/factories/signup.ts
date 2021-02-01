32aa01eeb697ddfc56d296457b5ab6883ddf63e0
Renan Fortunato <renanfortunato09@gmail.com>
2021-02-01 06:17:48 -0300

    feat: ensure ExpressRoutesAdapter returns the error message instead of th error name

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,6 +3,7 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { LogControllerDecorator } from '../decorators/log'
 
 export const makeSignUpController = (): SingUpController => {
@@ -12,5 +13,6 @@ export const makeSignUpController = (): SingUpController => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const singUpController = new SingUpController(emailValidatorAdapter, dbAddAccount)
-  return new LogControllerDecorator(singUpController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(singUpController, logMongoRepository)
 }
