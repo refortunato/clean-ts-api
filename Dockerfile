9a548e3b20993ef1f04b52f587744c0538fda0ac
Renan Fortunato <renanfortunato09@gmail.com>
2021-02-16 21:24:01 -0300

    chore: add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
-FROM node:15.4.0
+FROM node:15.8
 WORKDIR /usr/src/clean-node-api
 COPY ./package.json .
 RUN npm install --only-prod
 COPY ./dist ./dist
-EXPOSE 5000
-CMD npm start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'j58fryu5A4yEyg854=52Y34g5huyh'
 }
